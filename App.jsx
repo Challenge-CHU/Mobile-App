0c6960b766ade8cf6856a1245205890706c27a80
BoutroisJordan <jordanboutroispro@gmail.com>
2024-03-11 23:45:03 +0100

    push notif ios test ok

--- App.jsx
+++ App.jsx
@@ -27,9 +27,11 @@ import SignUp from "./screens/SignUp";
 import { useUserStore } from "./store/useUserStore";
 import AddPseudo from "./screens/ChangePseudo";
 import ImageLoader from "./components/ImageLoader";
+import React, { useEffect } from "react";
+import messaging from "@react-native-firebase/messaging";
+import * as Notifications from "expo-notifications";
 
 const Tab = createBottomTabNavigator();
-const ProfilStack = createStackNavigator();
 
 let customFonts = {
   // 'Poppins-Black': require('./assets/fonts/Poppins-Black.ttf')
@@ -46,6 +48,116 @@ let customFonts = {
 export default function App() {
   const [isLoaded] = useFonts(customFonts);
   useStepCount();
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      messaging()
+        .getToken()
+        .then((token) => console.log(token));
+    }
+    // }, []);
+
+    // // Set up the notification handler for the app
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+
+    // Handle user clicking on a notification and open the screen
+    const handleNotificationClick = async (response) => {
+      const screen = response?.notification?.request?.content?.data?.screen;
+      if (screen !== null) {
+        navigation.navigate(screen);
+      }
+    };
+
+    // Listen for user clicking on a notification
+    const notificationClickSubscription =
+      Notifications.addNotificationResponseReceivedListener(
+        handleNotificationClick
+      );
+
+    // Handle user opening the app from a notification (when the app is in the background)
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      console.log(
+        "Notification caused app to open from background state:",
+        remoteMessage.data.screen,
+        navigation
+      );
+      if (remoteMessage?.data?.screen) {
+        navigation.navigate(`${remoteMessage.data.screen}`);
+      }
+    });
+
+    // Check if the app was opened from a notification (when the app was completely quit)
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            "Notification caused app to open from quit state:",
+            remoteMessage.notification
+          );
+          if (remoteMessage?.data?.screen) {
+            navigation.navigate(`${remoteMessage.data.screen}`);
+          }
+        }
+      });
+
+    // Handle push notifications when the app is in the background
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+      const notification = {
+        title: remoteMessage.notification.title,
+        body: remoteMessage.notification.body,
+        data: remoteMessage.data, // optional data payload
+      };
+
+      // Schedule the notification with a null trigger to show immediately
+      await Notifications.scheduleNotificationAsync({
+        content: notification,
+        trigger: null,
+      });
+    });
+
+    // Handle push notifications when the app is in the foreground
+    const handlePushNotification = async (remoteMessage) => {
+      const notification = {
+        title: remoteMessage.notification.title,
+        body: remoteMessage.notification.body,
+        data: remoteMessage.data, // optional data payload
+      };
+
+      // Schedule the notification with a null trigger to show immediately
+      await Notifications.scheduleNotificationAsync({
+        content: notification,
+        trigger: null,
+      });
+    };
+
+    // Listen for push notifications when the app is in the foreground
+    const unsubscribe = messaging().onMessage(handlePushNotification);
+
+    // Clean up the event listeners
+    return () => {
+      unsubscribe();
+      notificationClickSubscription.remove();
+    };
+  }, []);
 
   return (
     <>
